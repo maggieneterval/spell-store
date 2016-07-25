7b6ac16331b431b642d327fe875ae15eada45106
Samantha Lowe <samanthalowe2010@gmail.com>
2016-07-25 17:59:52 -0400

    order and order detail models

--- server/db/index.js
+++ server/db/index.js
@@ -3,4 +3,10 @@ var db = require('./_db');
 module.exports = db;
 
 var User = require('./models/user');
+var Order = require('./models/order');
+var OrderDetail = require('./models/order_detail');
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
 
