c3f43a90fde23da8a2ead878d40b18c1ceb12f51
Samantha Lowe <samanthalowe2010@gmail.com>
2016-07-26 12:36:07 -0400

    started seed file

--- server/db/index.js
+++ server/db/index.js
@@ -4,14 +4,14 @@ module.exports = db;
 
 var User = require('./models/user');
 var Order = require('./models/order');
-var OrderDetail = require('./models/order_detail');
 var Product = require('./models/product');
 var Review = require('./models/review');
+var OrderDetail = require('./models/order_detail');
 
 User.hasMany(Order);
 Order.belongsTo(User);
 User.hasMany(Review);
 Review.belongsTo(User);
 Review.belongsTo(Product);
-Product.belongsToMany(Order, {as: 'OrderDetail'});
+Product.belongsToMany(Order, {through: OrderDetail});
 
