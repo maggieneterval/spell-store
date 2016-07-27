46ed9a140143b44965e3b54e272e5fcffae5317c
dany92 <dany@bc.edu>
2016-07-27 17:16:37 -0400

    review routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -6,6 +6,8 @@ router.use('/members', require('./members'));
 
 // Make sure this is after all of
 // the registered routes!
+
+router.use('/reviews', require('./reviews'));
 router.use(function (req, res) {
     res.status(404).end();
 });
