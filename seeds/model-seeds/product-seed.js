3963678185b15008eeb74dbbf776a49fb8ea9171
Dana Norwood <nimadera@nimadera.com>
2016-07-27 10:50:43 -0400

    restructured seed to split seeds for each model into separate files

--- seeds/model-seeds/product-seed.js
+++ seeds/model-seeds/product-seed.js
@@ -1,51 +1,5 @@
-/*
-
-This seed file is only a placeholder. It should be expanded and altered
-to fit the development of your application.
-
-It uses the same file the server uses to establish
-the database connection:
---- server/db/index.js
-
-The name of the database used is set in your environment files:
---- server/env/*
-
-This seed file has a safety check to see if you already have users
-in the database. If you are developing multiple applications with the
-fsg scaffolding, keep in mind that fsg always uses the same database
-name in the environment files.
-
-*/
-
-var chalk = require('chalk');
-var db = require('./server/db');
-var User = db.model('user');
+var db = require('../../server/db');
 var Product = db.model('product');
-var Promise = require('sequelize').Promise;
-
-var seedUsers = function () {
-
-    var users = [
-        {   
-            username: 'joseph',
-            email: 'testing@fsa.com',
-            password: 'password'
-        },
-        {
-            username: 'obama',
-            email: 'obama@gmail.com',
-            password: 'potus'
-        }
-    ];
-
-
-    var creatingUsers = users.map(function (userObj) {
-        return User.create(userObj);
-    });
-
-    return Promise.all(creatingUsers);
-
-};
 
 var seedProducts = function () {
 
@@ -119,7 +73,6 @@ var seedProducts = function () {
             inventory: 10
         }
     ];
-
    
     var creatingProducts = products.map(function (productObj) {
         return Product.create(productObj);
@@ -129,18 +82,4 @@ var seedProducts = function () {
 
 };
 
-db.sync({ force: true })
-    .then(function () {
-        return seedUsers();
-    })
-    .then(function () {
-        return seedProducts();
-    })
-    .then(function () {
-        console.log(chalk.green('Seed successful!'));
-        process.exit(0);
-    })
-    .catch(function (err) {
-        console.error(err);
-        process.exit(1);
-    });
+module.exports = seedProducts;
