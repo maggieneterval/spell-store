0aafe5421b16063d11e2b311b43375f8834cd500
dany92 <dany@bc.edu>
2016-07-28 16:26:11 -0400

    updated

--- browser/js/common/states/product-reviews/product-reviews.js
+++ browser/js/common/states/product-reviews/product-reviews.js
@@ -1,7 +1,8 @@
 app.config(function ($stateProvider) {
   $stateProvider.state('productReviews', {
+    //Should this be nested under the product details state?
     url: '/products/:productId/reviews',
-    templateUrl: '/js/common/states/all-reviews/reviews.html',
+    templateUrl: '/js/common/states/product-reviews/product-reviews.html',
     controller: function ($scope, ReviewFactory, $stateParams) {
       ReviewFactory.fetchAll({productId: $stateParams.productId})
       .then(function(reviews){
