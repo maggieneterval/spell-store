a8692fc507ecb8eb59cfccb852bc832884621d43
Maggie Neterval <mneterval@gmail.com>
2016-07-30 17:46:08 -0400

    started admin ctrl test

--- browser/js/common/states/admin/manage-products/admin.manageProducts.state.js
+++ browser/js/common/states/admin/manage-products/admin.manageProducts.state.js
@@ -7,33 +7,35 @@ app.config(function ($stateProvider) {
                 return ProductsFactory.fetchAll();
             }
         },
-        controller: function ($scope, products, ProductsFactory) {
-            $scope.products = products;
-            $scope.selectedProduct;
-            $scope.newProduct;
-            $scope.submitProductForm = function () {
-                return ProductsFactory.updateProduct($scope.selectedProduct.id, $scope.selectedProduct)
-                .then(function () {
-                    $scope.selectedProduct = null;
-                    alert('Successfully updated product record.');
-                })
-            }
-            $scope.deleteProduct = function () {
-                var indOfDeleted = $scope.products.indexOf($scope.selectedProduct);
-                $scope.products.splice(indOfDeleted, 1);
-                return ProductsFactory.deleteProduct($scope.selectedProduct.id)
-                .then(function () {
-                    $scope.selectedProduct = null;
-                    alert('Successfully deleted product.');
-                })
-            }
-            $scope.submitNewProduct = function () {
-                return ProductsFactory.createProduct($scope.newProduct)
-                .then(function () {
-                    $scope.newProduct = null;
-                    alert('Successfully created new product!')
-                })
-            }
-        }
+        controller: 'AdminProductCtrl'
     })
-})
+});
+
+app.controller('AdminProductCtrl', function ($scope, products, ProductsFactory) {
+    $scope.products = products;
+    $scope.selectedProduct;
+    $scope.newProduct;
+    $scope.submitProductForm = function () {
+        return ProductsFactory.updateProduct($scope.selectedProduct.id, $scope.selectedProduct)
+        .then(function () {
+            $scope.selectedProduct = null;
+            alert('Successfully updated product record.');
+        })
+    }
+    $scope.deleteProduct = function () {
+        var indOfDeleted = $scope.products.indexOf($scope.selectedProduct);
+        $scope.products.splice(indOfDeleted, 1);
+        return ProductsFactory.deleteProduct($scope.selectedProduct.id)
+        .then(function () {
+            $scope.selectedProduct = null;
+            alert('Successfully deleted product.');
+        })
+    }
+    $scope.submitNewProduct = function () {
+        return ProductsFactory.createProduct($scope.newProduct)
+        .then(function () {
+            $scope.newProduct = null;
+            alert('Successfully created new product!')
+        })
+    }
+});
---
+++ tests/browser/controllers/admin-manage-products-test.js
@@ -0,0 +1,32 @@
+// 'use strict';
+
+// var expect = chai.expect;
+
+// describe('adminProductCtrl controller', function () {
+
+//     var $controller, myController;
+
+//     beforeEach(module('FullstackGeneratedApp'));
+
+//     beforeEach(inject(function (_$controller_, UsersFactory, products) {
+
+//         this.products = products;
+//         $controller = _$controller_;
+//         myController = $controller('AdminProductCtrl', { $scope: $scope, products: this.products });
+
+//     }));
+
+//     describe('$scope.submitProductForm', function () {
+
+//         it('resets $scope.selectedProduct to null', function () {
+
+//             var $scope = {};
+//             $scope.selectedProduct = {title: 'something'};
+//             $scope.submitProductForm();
+//             expect($scope.selectedProduct).toBe(null);
+//         })
+//     })
+
+// })
+
+
