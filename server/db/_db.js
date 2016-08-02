ab0d95b3a7b66b9f45cad8913e8ea9e04335169a
Maggie Neterval <mneterval@gmail.com>
2016-08-02 16:06:01 -0400

    heroku test

--- server/db/_db.js
+++ server/db/_db.js
@@ -3,7 +3,7 @@ var Sequelize = require('sequelize');
 
 var env = require(path.join(__dirname, '../env'));
 //change to URL when deploying with Heroku:
-// var db = new Sequelize(env.DATABASE_URL, { logging: env.LOGGING });
-var db = new Sequelize(env.DATABASE_URI, { logging: env.LOGGING });
+var db = new Sequelize(env.DATABASE_URL, { logging: env.LOGGING });
+// var db = new Sequelize(env.DATABASE_URI, { logging: env.LOGGING });
 
 module.exports = db;
