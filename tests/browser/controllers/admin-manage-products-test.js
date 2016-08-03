861e1100be4651a02622185163725fc9c398a404
Maggie Neterval <mneterval@gmail.com>
2016-08-03 11:38:48 -0400

    deployment

--- tests/browser/controllers/admin-manage-products-test.js
+++ tests/browser/controllers/admin-manage-products-test.js
@@ -1,32 +1,63 @@
-// 'use strict';
+'use strict';
 
-// var expect = chai.expect;
+var expect = chai.expect;
 
-// describe('adminProductCtrl controller', function () {
+var mockProducts = [
+    {
+    title: 'Amplifying Charm',
+    category: 'Charms',
+    description: ' A charm that can be used to amplify the targeted sound, be it a person\'s voice or a piece of equipment.',
+    price: 500,
+    deliverable: 'Sonorus',
+    inventory: 20
 
-//     var $controller, myController;
+    },
+    {
+        title: 'Fire-Making Spell',
+        category: 'Transfigurations',
+        description: 'A form of Conjuration that can be used to conjure a jet of orange and red flame, thereby setting things alight',
+        price: 1000,
+        deliverable: 'Incendio',
+        inventory: 20
 
-//     beforeEach(module('FullstackGeneratedApp'));
+    },
+    {
+        title: 'Stickfast Hex',
+        category: 'Hexes',
+        price: 3000,
+        description: 'A hex that was used to make the target\'s shoes stick to the ground.',
+        deliverable: 'Colloshoo',
+        inventory: 35
+    }
+];
 
-//     beforeEach(inject(function (_$controller_, UsersFactory, products) {
+describe('adminProductCtrl controller', function () {
 
-//         this.products = products;
-//         $controller = _$controller_;
-//         myController = $controller('AdminProductCtrl', { $scope: $scope, products: this.products });
+    var $controller, myController, $scope, ProductsFactory;
 
-//     }));
+    beforeEach(module('FullstackGeneratedApp', function ($provide) {
+        $provide.value('products', mockProducts)
+    }));
 
-//     describe('$scope.submitProductForm', function () {
+    beforeEach(inject(function (_$controller_, UsersFactory, products, ProductsFactory) {
 
-//         it('resets $scope.selectedProduct to null', function () {
+        $controller = _$controller_;
+        products = products;
+        $scope = {};
+        ProductsFactory = ProductsFactory;
 
-//             var $scope = {};
-//             $scope.selectedProduct = {title: 'something'};
-//             $scope.submitProductForm();
-//             expect($scope.selectedProduct).toBe(null);
-//         })
-//     })
+        myController = $controller('AdminProductCtrl', { $scope: $scope, products: products });
 
-// })
+    }));
+
+    describe('$scope.submitProductForm', function () {
+
+        it('$scope.selectedProduct starts as undefined', function () {
+
+            expect($scope.selectedProduct).to.equal(undefined);
+        })
+    })
+
+})
 
 
